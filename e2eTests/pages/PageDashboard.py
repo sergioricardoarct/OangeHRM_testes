e45a77a63e2fb3869169f6b04dd02e1d844ef84d
sergio <sergioricardo.arct@gmail.com>
2023-07-29 20:07:19 -0300

    OrangeHRM == 29-07-2023 - 20:07 - S

--- e2eTests/pages/PageDashboard.py
+++ e2eTests/pages/PageDashboard.py
@@ -1,24 +1,24 @@
 from e2eTests.pages.PageObject import PageObject
 from selenium.webdriver.common.by import By
-from e2eTests.locators import LocatorsAdmin
+from e2eTests.locators import LocatorsDashboard
 from selenium.webdriver.common.by import By
 
-from e2eTests.locators import LocatorsAdmin
+from e2eTests.locators import LocatorsDashboard
 from e2eTests.pages.PageObject import PageObject
 
 
-class PageAdmin(PageObject):
+class PageDashboard(PageObject):
     # SETUP
     base_url = 'https://opensource-demo.orangehrmlive.com/'
 
     def __init__(self, driver=None, browser='chorme'):
         super().__init__(driver, browser=browser)
-        self.opemAdmin()
+        self.openDashboard()
 
-    def opemAdmin(self):
+    def openDashboard(self):
         self.driver.get(self.base_url)
 
     # SET
 
     def clicPIM(self):
-        self.driver.find_element(By.XPATH, LocatorsAdmin.botao_pim_xpath).click()
+        self.driver.find_element(By.XPATH, LocatorsDashboard.botao_pim_xpath).click()
