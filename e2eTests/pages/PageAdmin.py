113e1d1240a1ea5bc3b37aa05436ec39188eccd5
sergio <sergioricardo.arct@gmail.com>
2023-07-29 19:57:47 -0300

    OrangeHRM == 29-07-2023 - 20:00 - S

--- e2eTests/pages/PageAdmin.py
+++ e2eTests/pages/PageAdmin.py
@@ -1,17 +1,24 @@
-from selenium import webdriver
+from e2eTests.pages.PageObject import PageObject
+from selenium.webdriver.common.by import By
+from e2eTests.locators import LocatorsAdmin
+from selenium.webdriver.common.by import By
+
+from e2eTests.locators import LocatorsAdmin
 from e2eTests.pages.PageObject import PageObject
 
-class PageAdmin(PageObject):
 
-# SETUP
+class PageAdmin(PageObject):
+    # SETUP
     base_url = 'https://opensource-demo.orangehrmlive.com/'
 
-    def __init__(self,driver=None, browser='chome'):
+    def __init__(self, driver=None, browser='chorme'):
         super().__init__(driver, browser=browser)
         self.opemAdmin()
 
     def opemAdmin(self):
         self.driver.get(self.base_url)
 
-#SET
+    # SET
 
+    def clicPIM(self):
+        self.driver.find_element(By.XPATH, LocatorsAdmin.botao_pim_xpath).click()
