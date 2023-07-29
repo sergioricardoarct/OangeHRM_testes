7f3db7f4ccf6000806baeb4362ce22185877aa7c
sergio <sergioricardo.arct@gmail.com>
2023-07-29 20:13:29 -0300

    OrangeHRM == 29-07-2023 - 20:13 - S

---
+++ e2eTests/pages/PageEmployeeInformation.py
@@ -0,0 +1,14 @@
+from e2eTests.pages.PageObject import PageObject
+from selenium.webdriver.common.by import By
+from e2eTests.locators import LocatorsEmployee
+from selenium.webdriver.common.by import By
+
+from e2eTests.locators import LocatorsDashboard
+from e2eTests.pages.PageObject import PageObject
+
+
+class PageEmployee(PageObject):
+    def __init__(self,driver):
+        super().__init__(driver)
+
+    def
