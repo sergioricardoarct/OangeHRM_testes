55eb28b591c42d4ba3a1dc0f24f796e9422e1e9c
sergio <sergioricardo.arct@gmail.com>
2023-07-31 18:51:01 -0300

    OrangeHRM == 31-07-2023 - 18:50 - S

--- e2eTests/pages/PageEmployeeInformation.py
+++ e2eTests/pages/PageEmployeeInformation.py
@@ -15,3 +15,12 @@ class PageEmployee(PageObject):
 #SETs
     def set_Nane(self,Nome):
         self.driver.find_element(By.XPATH, LocatorsAddEmployee.input_first_nome_xpath).send_keys(Nome)
+    def set_Middle(self,Middle):
+        self.driver.find_element(By.XPATH, LocatorsAddEmployee.input_middle_nome_xpath).send_keys(Middle)
+    def set_Last(self,Last):
+        self.driver.find_element(By.XPATH, LocatorsAddEmployee.input_last_nome_xpath).send_keys(Last)
+    def set_Id(self,Id):
+        self.driver.find_element(By.XPATH, LocatorsAddEmployee.id_employee_xpath).send_keys(Id)
+    def Click_Save(self):
+        self.driver.find_element(By.XPATH,LocatorsAddEmployee.button_save_xpath).click()
+
