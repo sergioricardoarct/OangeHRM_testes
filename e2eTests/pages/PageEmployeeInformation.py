f71b3d50741f05dfb443608a7dd3b3b174164a77
sergio <sergioricardo.arct@gmail.com>
2023-07-31 18:24:43 -0300

    OrangeHRM == 31-07-2023 - 18:24 - S

--- e2eTests/pages/PageEmployeeInformation.py
+++ e2eTests/pages/PageEmployeeInformation.py
@@ -1,14 +1,17 @@
 from e2eTests.pages.PageObject import PageObject
 from selenium.webdriver.common.by import By
 from e2eTests.locators import LocatorsEmployee
+from e2eTests.locators import LocatorsAddEmployee
 from selenium.webdriver.common.by import By
 
 from e2eTests.locators import LocatorsDashboard
 from e2eTests.pages.PageObject import PageObject
 
-
+# INIT
 class PageEmployee(PageObject):
     def __init__(self,driver):
         super().__init__(driver)
 
-    def z
+#SETs
+    def set_Nane(self,Nome):
+        self.driver.find_element(By.XPATH, LocatorsAddEmployee.input_first_nome_xpath).send_keys(Nome)
