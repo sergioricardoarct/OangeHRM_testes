712cad5dac8f6cf4100e92ef6aff2654bb79a584
sergio <sergioricardo.arct@gmail.com>
2023-08-08 19:26:31 -0300

    OrangeHRM == 08-08-2023 - 19:30 - S

--- e2eTests/tests/Cadastro_Test.py
+++ e2eTests/tests/Cadastro_Test.py
@@ -1,11 +1,18 @@
 from time import time
 
 from e2eTests.pages.IndexPage import PageIndex
+import e2eTests.pages.PageDashboard
 
 
-class Test_Cadastro:
+class Cadastro_Test:
 
-    def test_abrir_inicial(self, open_index):
-        pageIndex = PageIndex(driver=open_index)
-        pageIndex.open_Index()
-        time(10)
+    def test_abrir_inicial(self, open_index, PageDashboard):
+        pageIndex = open_index(driver=open_index.driver)
+        pageIndex.username()
+        open_index.password()
+        open_index.click_login()
+
+        PageDashboard = PageDashboard(driver=PageDashboard.driver)
+        time(5)
+        PageDashboard.clicPIM()
+        assert PageDashboard
--- e2eTests/tests/configtests.py
+++ e2eTests/tests/configtests.py
@@ -2,11 +2,13 @@ import pytest
 from e2eTests.pages.IndexPage import PageIndex
 
 @pytest.fixture()
-def Index_Open():
+def Index_Open(PageIndex):
     print('Login!')
-    open_index = PageIndex(brower='chrome')
+    open_index = PageIndex.driver(brower='chrome')
+
     open_index.username('Admin')
     open_index.password('Adm123')
     open_index.click_login()
+
     yield open_index
-    open_index.close()
+
